f21ef2c675747ec1743e316dd42413d318c77a8f
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-10-05 09:19:19 -0400

    s

--- Project 1/src/dWang.java
+++ Project 1/src/dWang.java
@@ -1,4 +1,10 @@
 
 public class dWang {
-
+// date str, is paliendrome, sumupto
+	public static String datestr ( String a ) {
+		String month= a.substring (0,2);
+		String day= a.substring(3,5);
+		String year= a.substring(6,10);
+		return (day + "-" + month + "-" + year);
+	}
 }
