2433d734ce96897944d9d13cac8fb78cfeccab57
User <User@User-PC.home>
2017-10-09 16:21:12 -0400

    Multiplication Table Finish

--- Project 1/src/MutiplicationTable.java
+++ Project 1/src/MutiplicationTable.java
@@ -1,4 +1,40 @@
 //Xing Chen
 public class MutiplicationTable {
-
+	public static void main(String []args) 
+	{
+		multiplicationTable(12,12);
+	}	
+public static void 	multiplicationTable(int base,int limit)
+{
+	int x=0; 
+	int y=1;
+	while ( x!=limit )
+	{
+		System.out.print( base*(limit-limit+y)+ " " );
+		x++;
+		y++;
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
