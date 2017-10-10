bdb6189ed036f6a02be005193fdccc6380801f38
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-10-10 09:27:21 -0400

    Moved all into 1

--- Project 1/src/MutiplicationTable.java
+++ Project 1/src/MutiplicationTable.java
@@ -3,21 +3,46 @@ public class MutiplicationTable {
 	public static void main(String []args) 
 	{
 		multiplicationTable(12,12);
+		if ( isFibonacci(5) )
+		System.out.println( "Yes");
 	}	
 public static void 	multiplicationTable(int base,int limit)
 {
-	int x=0; 
+
 	int y=1;
-	while ( x!=limit )
+	while ( y!=limit+1 )
 	{
 		System.out.print( base*(y)+ " " );
-		x++;
 		y++;
 	}
-	
+	System.out.println(" ");
+
 }
 	
+public static boolean isFibonacci(int num)
+{
+	int x;
+	int y;
+	if ( num <= 3 )
+		return true;
+	for (x=1,y=1; x>=num; x+=y, y+=x  )
+	{
+		x+=4;
+		return true; 
+	}
+	return true;
+}
+
+
+
+
+
+
+
+public static void cutOut() //void?
+{
 	
+}
 	
 	
 	
