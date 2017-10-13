8dc75f432c5db5d28701dce94b9de91d6802c471
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2017-10-12 21:40:21 -0400

    Update dWang.java

--- Project 1/src/dWang.java
+++ Project 1/src/dWang.java
@@ -1,14 +1,21 @@
 
 public class dWang {
-// date str, is paliendrome, sumupto
-	public static String datestr ( String a ) {
+	public static String dateStr ( String a ) {
 		String month= a.substring (0,2);
 		String day= a.substring(3,5);
 		String year= a.substring(6,10);
 		return (day + "-" + month + "-" + year);
 	}
-	public static void sumUpTo (int n) {
-		  System.out.println(n * (n + 1) / 2);
-		    }
-	
+	public static int sumUpTo (int n) {
+		  int x = (n * (n + 1) / 2); 
+		  return (x);
+		  
+		}
+	public static boolean isPalindrome(String x) {
+		String word= "";
+			for (int n = x.length() -1; n == 0; n--) {
+				word = word + x.charAt(n);
+			}
+				return (x.equals(word));
+	}	
 }
