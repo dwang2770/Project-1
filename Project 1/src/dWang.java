7e6200e816783b1592f15108e1e1500f08a0cfdb
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2017-10-17 09:29:52 -0400

    Update dWang.java

--- Project 1/src/dWang.java
+++ Project 1/src/dWang.java
@@ -5,11 +5,13 @@ public class dWang {
 		String day= a.substring(3,5);
 		String year= a.substring(6,10);
 		return (day + "-" + month + "-" + year);
+	// takes a date in the given form mm//dd/yyyy to dd-mm-yyyy
+	// using substrings to isolate mm, dd, yyyy and rearranging them to the new format
 	}
 	public static int sumUpTo (int n) {
 		  int x = (n * (n + 1) / 2); 
 		  return (x);
-		  
+	// using a mathematical formula to calculate the sum of all numbers up to input value	  
 		}
 	public static boolean isPalindrome(String x) {
 		String word= "";
@@ -17,5 +19,8 @@ public class dWang {
 				word = word + x.charAt(n);
 			}
 				return (x.equals(word));
+	//by using a for loop you can set a blank string to equal its reverse
+		// this is done by having the for loop count down and having the charAt(0)
+	// then you check if the reverse string and original string is equal to each other
 	}	
 }
